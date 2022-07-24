43fda93ee3adc2143006b4aab928481c2f3e0dc3
Emmanuel <70802541+MenuItem207@users.noreply.github.com>
2022-07-24 15:32:44 +0800

    variable explanation

--- lib/src/00_variables.dart
+++ lib/src/00_variables.dart
@@ -1,6 +1,17 @@
 void main() {
+  // a variable is a container for a value
+  // you can think of a variable as a sort of container that only allows one type of value.
+  // so a int container can only store int
+  // so when I say something like
+  int newInstance = 5;
+  // I'm creating a new container called newInstance that only stores int values and currently stores the value 5
+  // so when I say something else like
+  int otherInstance = 8;
+  // I created a separate container known as otherInstance that only stores int values and currently stores the value 8
+  // So these 'containers' are known as instances. Each is its own instance.
+
   // basic variables types
-// int, double, String, bool
+  // int, double, String, bool
   int a = 5;
   double b = 5.5;
   String c = '';
@@ -31,4 +42,20 @@ void main() {
   bool
       notNullableBool; // must have a value before its used i.e bool notNullableBool = true;
   bool? nullableBool; // does not need a value
+
+  // there are others like List and Map
+  // what is a List
+  List mylist = [
+    1, // index 0
+    2, // index 1
+    3,
+    4,
+    5
+  ]; // a list is just a sequential collection of data
+  // each list element (1,2,3,4,5) has a position, starting from zero
+  // so position zero = 1, position one = 2
+  // i.e mylist[0] results in a value of one because the first index (0), has a value of one
+  int indexZero = 0;
+  mylist[indexZero]; // *one
+  // removeAt removesAT the INDEX. so calling mylist.removeAt(0) will remove the number one.
 }
---
+++ lib/src/01_functions2.dart
@@ -0,0 +1,69 @@
+/// in this section we'll discuss variable scopes
+
+// I have a variable called abc here with the value of 5
+int abc = 5;
+
+void doSomething() {
+  abc = abc + 5; // tip: you can also do abc += 5;
+}
+
+// what would calling this function do? i.e:
+// void main() {
+//   doSomething();
+//   print(abc); // 5
+// }
+// it will add 5 to abc so abc would be = 5
+
+int abcd = 1;
+
+void doSomethingElse() {
+  int abcd = 8;
+  abcd += 2;
+}
+
+// what would calling this new function do?
+// void main() {
+//   doSomethingElse();
+//   print(abcd); // what would the value here be
+// }
+
+// the answer is NOT 10
+// the answer is 1
+// why did doSomethingElse not change the value of abcd?
+// variable initialised in a function are LOCAL
+// variables initialised outside of a function are GLOBAL
+// basically, GLOBAL variables can be accessed and modified anywhere
+// LOCAL variables exist only within their SCOPE (function)
+// so when I do
+int abcde =
+    1; // I created a GLOBAL variable because this variable is not within any function
+
+void doSomethingElseAgain() {
+  int abcde =
+      5; // i created a LOCAL variable within the function. This LOCAL variable instance exists ONLY within this function
+  // in this function there are currently TWO instance of abcde. One is the GLOBAL abcde and one is the LOCAL abcde
+  // which abcde will be modifed if I do something like this:
+  abcde += 8;
+  // the LOCAL one.
+  // Because if there are GLOBAL variables with the same name, only the LOCAL variable will be valid
+}
+
+// void main() {
+//   abcde += 3; // which instance is it referring to? GLOBAL or LOCAL
+//   // GLOBAL
+// }
+
+// remember, a LOCAL variable only exists within its scope
+int a = 5; // a can be accessed anywhere
+void main() {
+  print(a);
+  int b = 6; // b can only be accesssed within this function
+}
+
+print(b); // can't do this because b only exists within the curly braces of main()
+
+void doSomethingElseOnceAgain() {
+  int c = 7; // c can only be accessed within this function
+  print(b); // b doesn't exist here, only within main()
+  print(a); // a is GLOBAL
+}
